e39ad0e025bac6f7a3922cddcb8d6c163e230285
kotaro0522 <kotaro0522@users.noreply.github.com>
2015-12-16 12:22:33 +0900

    Update patternmatching.c

--- patternmatching.c
+++ patternmatching.c
@@ -13,8 +13,23 @@ int main(void){
         fgets(Mmap[i], sizeof(Mmap[i]), stdin);
     }
 
-    for(int i=0;i<M;i++){
-        printf("%s", Mmap[i]);
+    for(int Ny=0;Ny<N-M+1;Ny++){
+    next:
+        for(int Nx=0;Nx<(N-M+1)*2;Nx+=2){
+            for(int My=0;My<M;My++){
+                for(int Mx=0;Mx<M*2;Mx+=2){
+                    if(Nmap[Ny][Nx] != Mmap[My][Mx]){
+                        goto next;
+                    }
+                    if(M==My+1&&M==Mx/2+1){
+                        printf("%d %d", );
+                    }
+                    Nx++;
+                }
+                Ny++;
+            }
+            
+        }
     }
 
     return 0;
